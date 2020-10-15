a97816e33b3a38a3ce8a1fc4ab37c56c3953107f
Nathan Hensel <nathanihensel@gmail.com>
2020-10-15 12:12:03 -0500

    template database connections

--- template.py
+++ template.py
@@ -12,3 +12,31 @@ output = template.render(keystore_password=os.environ['SHRINE_KEYSTORE_PASSWORD'
 f = open("/usr/local/tomcat/lib/shrine.conf", "w")
 f.write(output)
 f.close()
+
+template = env.get_template('context.xml.j2')
+# there must be a better way to do this
+output = template.render(shrineDatabaseType=os.environ['SHRINE_DB_TYPE'], # noqa
+                         problemDB_user=os.environ['SHRINE_PROBLEMDB_USER'], # noqa
+                         problemDB_pass=os.environ['SHRINE_PROBLEMDB_PASS'], # noqa
+                         problemDB_driver=os.environ['SHRINE_PROBLEMDB_DRIVER'], # noqa
+                         problemDB_url=os.environ['SHRINE_PROBLEMDB_URL'], # noqa
+                         shrineDB_user=os.environ['SHRINE_SHRINEDB_USER'], # noqa
+                         shrineDB_pass=os.environ['SHRINE_SHRINEDB_PASS'], # noqa
+                         shrineDB_driver=os.environ['SHRINE_SHRINEDB_DRIVER'], # noqa
+                         shrineDB_url=os.environ['SHRINE_SHRINEDB_URL'], # noqa
+                         adapterAuditDB_user=os.environ['SHRINE_ADAPTERAUDITDB_USER'], # noqa
+                         adapterAuditDB_pass=os.environ['SHRINE_ADAPTERAUDITDB_PASS'], # noqa
+                         adapterAuditDB_driver=os.environ['SHRINE_ADAPTERAUDITDB_DRIVER'], # noqa
+                         adapterAuditDB_url=os.environ['SHRINE_ADAPTERAUDITDB_URL'], # noqa
+                         qepAuditDB_user=os.environ['SHRINE_QEPAUDITDB_USER'], # noqa
+                         qepAuditDB_pass=os.environ['SHRINE_QEPAUDITDB_PASS'], # noqa
+                         qepAuditDB_driver=os.environ['SHRINE_QEPAUDITDB_DRIVER'], # noqa
+                         qepAuditDB_url=os.environ['SHRINE_QEPAUDITDB_URL'], # noqa
+                         stewardDB_user=os.environ['SHRINE_STEWARDDB_USER'], # noqa
+                         stewardDB_pass=os.environ['SHRINE_STEWARDDB_PASS'], # noqa
+                         stewardDB_driver=os.environ['SHRINE_STEWARDDB_DRIVER'], # noqa
+                         stewardDB_url=os.environ['SHRINE_STEWARDDB_URL']
+                        )
+f = open("/usr/local/tomcat/conf/context.xml", "w")
+f.write(output)
+f.close()
